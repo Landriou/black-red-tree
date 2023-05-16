04426039fdfb7df830f6e18b22136878c47fa629
Landriou <39389586+Landriou@users.noreply.github.com>
2023-05-16 00:02:58 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from redblacktree import *
+
+B = RedBlackTree()
+
+
+r_insert(B,1,8)
+r_insert(B,1,5)
+r_insert(B,1,15)
+r_insert(B,1,19)
+r_insert(B,1,12)
+r_insert(B,1,23)
+r_insert(B,1,9)
+r_insert(B,1,13)
+r_insert(B,1,10)
+imprimirarbol__P(B, B.root)
